0aaeb450a3cc0847189ea1fc6f2cb9eb655267dd
Homework <sin.kostya.1999@gmail.com>
2022-11-10 19:37:30 +0300

    fix task-1 without users friendship entity

--- service/src/main/java/org/itacademy/homework/service/command/user_command/ShowUserPosts.java
+++ service/src/main/java/org/itacademy/homework/service/command/user_command/ShowUserPosts.java
@@ -1,12 +1,12 @@
 package org.itacademy.homework.service.command.user_command;
 
-import org.itacademy.homework.data.ProfileList;
-import org.itacademy.homework.exception.IncorrectUserIdInputException;
-import org.itacademy.homework.exception.InvalidTypeEnteredException;
-import org.itacademy.homework.model.Message;
-import org.itacademy.homework.model.Profile;
 import org.itacademy.homework.service.command.Command;
 import org.itacademy.homework.service.command.CommandHandler;
+import org.itacademy.homework.data.ProfileList;
+import org.itacademy.homework.service.exception.IncorrectUserIdInputException;
+import org.itacademy.homework.service.exception.InvalidTypeEnteredException;
+import org.itacademy.homework.model.Message;
+import org.itacademy.homework.model.Profile;
 
 import java.util.Scanner;
 
